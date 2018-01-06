900d36345e3db172b41f301498e2798f2902e81c
Akira Kido <achts.y@gmail.com>
2018-01-06 10:22:12 +0900

    initial commit

---
+++ src/NcvPluginSample/Main.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using Plugin;
+
+namespace NcvPluginSample
+{
+    public class Main : IPlugin
+    {
+        private readonly Queue<string> _comments = new Queue<string>();
+
+        private IWebDriver _driver;
+
+        public void AutoRun() => Run();
+
+        public void Run()
+        {
+            if(_driver != null) return;
+
+            var driver = new ChromeDriver
+            {
+                Url = "https://translate.google.co.jp/#auto/ja/"
+            };
+            _driver = driver;
+
+            Host.ReceivedComment += Host_ReceivedComment;
+            Host.BroadcastDisConnected += Host_BroadcastDisConnected;
+
+            Task.Run( async () =>
+            {
+                while (true)
+                {
+                    if (_comments.Any())
+                    {
+                        var comment = _comments.Dequeue();
+                        await MakeGoogleSpeak(comment);
+                    }
+
+                    await Task.Delay(2000);
+                }
+            });
+        }
+
+        private void Host_BroadcastDisConnected(object sender, System.EventArgs e)
+        {
+            _driver.Dispose();
+        }
+
+        private void Host_ReceivedComment(object sender, ReceivedCommentEventArgs e)
+        {
+            var comment = e.CommentDataList.First().Comment;
+            _comments.Enqueue(comment);
+        }
+
+        private async Task MakeGoogleSpeak(string message)
+        {
+            var textbox = _driver.FindElement(By.Id("source"));
+            textbox.Clear();
+            textbox.SendKeys(message);
+            await Task.Delay(1000);
+            var gtspeech = _driver.FindElement(By.XPath("//*[@id=\"gt-src-listen\"]"));
+            var action = new Actions(_driver).Click(gtspeech).Build();
+            action.Perform();
+        }
+
+        public IPluginHost Host { get; set; }
+        public bool IsAutoRun { get; } = true;
+        public string Description { get; } = "グーグルがコメントを読み上げてくれます。";
+        public string Version { get; } = "0.1";
+        public string Name { get; } = "グーグル読み上げさん";
+    }
+}
---
+++ src/SeleniumTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
+namespace SeleniumTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new ChromeDriver
+            {
+                Url = "https://translate.google.co.jp/#ja/ja/"
+            };
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+
+            var textbox = driver.FindElement(By.Id("source"));
+            textbox.SendKeys("hogehoge");
+            Thread.Sleep(1000);
+            var gtspeech = driver.FindElement(By.XPath("//*[@id=\"gt-src-listen\"]"));
+            var action = new Actions(driver).Click(gtspeech).Build();
+            action.Perform();
+            //gtspeech.Click();
+        }
+    }
+}
